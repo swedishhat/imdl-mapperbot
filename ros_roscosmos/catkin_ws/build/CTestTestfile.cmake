5557b42f0ecd88818f88504ddb2780b45e5b8e1c
Patrick Lloyd <patrick.d.lloyd@gmail.com>
2014-08-31 15:20:01 -0400

    added some ros things for roscosmos. uhhhh

---
+++ ros_roscosmos/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jimbo/Projects/imdl-mapperbot/ros_roscosmos/catkin_ws/src
+# Build directory: /home/jimbo/Projects/imdl-mapperbot/ros_roscosmos/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
